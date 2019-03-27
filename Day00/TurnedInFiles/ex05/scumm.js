c51316570ba79eeae2a9df98a2e289123de88039
jcope <thejemmeh@gmail.com>
2019-03-26 21:57:06 -0700

    Day00 Ex05 3 pts. Can now grab things and have it show in inventory

--- Day00/TurnedInFiles/ex05/scumm.js
+++ Day00/TurnedInFiles/ex05/scumm.js
@@ -1,4 +1,6 @@
 
+var currentSelectedItem;
+
 function changeLocation(){
 	element=document.getElementById('locationimg')
 	if (element.src.match("robotics")) {
@@ -9,18 +11,39 @@ function changeLocation(){
     	document.getElementById("locationimg").useMap = "#ClickableCluster";
     }else{
         document.getElementById("locationimg").src = "resources/robotics.jpg";
+        document.getElementById("locationimg").useMap = "#ClickableRobotics";
     }
 }
 
 function talkTo(Name){
 	if(Name == 'Jamie'){
-		document.getElementById("GreenBoxText").innerHTML = "Hi, I'm Jamie! I'm the corporate relations manager. Whats your name?";
+		document.getElementById("GreenBoxText").innerHTML = "Hi, I'm Jamie! I'm the corporate relations manager. You should check out the golf cart in the robotics lab! Whats your name?";
 	}
 	if(Name == 'Student'){
-		document.getElementById("GreenBoxText").innerHTML = "Hi, I'm a student here at 42. Are you thinking of attending?";
+		document.getElementById("GreenBoxText").innerHTML = "Hi, I'm a student here at 42. I could use some coffee. Are you thinking of attending 42?";
 	}
 }
 
-function selectedItem(){
+function highlightSelectedItem(itemID){
+	if(currentSelectedItem){
+		document.getElementById(currentSelectedItem).className = "actions";
+	}
+	document.getElementById(itemID).className = "actions selected";
+	currentSelectedItem = itemID;
+}
+
+function GrabItem(itemTaken){
+	if(itemTaken == 'GolfCart' && currentSelectedItem == 'Take'){
+		document.getElementById("GreenBoxText").innerHTML = "You take the golf cart.<br/> ...Why? Are you planning to drive to Tesla?";
+		document.getElementById("cart").style.visibility = "visible";
+	}
+	else if(itemTaken == 'Cup' && currentSelectedItem == 'Take'){
+		document.getElementById("GreenBoxText").innerHTML = "You snatch the coffee out of the programmer's hands. The programmer does not seem happy about it.";
+		document.getElementById("cup").style.visibility = "visible";
+	}
+	else{
+		document.getElementById("GreenBoxText").innerHTML = "To take things you need to click the take button first.";
+		document.getElementById(itemTaken).style.visibility = "visible";
+	}
 
 }
