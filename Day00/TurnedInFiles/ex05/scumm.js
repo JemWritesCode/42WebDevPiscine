105b19cdbd20ca1cb93420b9fd598470276865d7
jcope <thejemmeh@gmail.com>
2019-03-26 20:52:06 -0700

    Day00 Ex05 2pts

--- Day00/TurnedInFiles/ex05/scumm.js
+++ Day00/TurnedInFiles/ex05/scumm.js
@@ -1,4 +1,26 @@
+
 function changeLocation(){
-	if (document.getElementById("imgClickAndChange").src == "http://www.userinterfaceicons.com/80x80/minimize.png")
+	element=document.getElementById('locationimg')
+	if (element.src.match("robotics")) {
+        document.getElementById("locationimg").src = "resources/checkinroom.jpg";
+        document.getElementById("locationimg").useMap = "#ClickableCheckin";
+    } else if (element.src.match("checkinroom")) {
+    	document.getElementById("locationimg").src = "resources/cluster.jpg";
+    	document.getElementById("locationimg").useMap = "#ClickableCluster";
+    }else{
+        document.getElementById("locationimg").src = "resources/robotics.jpg";
+    }
+}
+
+function talkTo(Name){
+	if(Name == 'Jamie'){
+		document.getElementById("GreenBoxText").innerHTML = "Hi, I'm Jamie! I'm the corporate relations manager. Whats your name?";
+	}
+	if(Name == 'Student'){
+		document.getElementById("GreenBoxText").innerHTML = "Hi, I'm a student here at 42. Are you thinking of attending?";
+	}
+}
+
+function selectedItem(){
 
 }
