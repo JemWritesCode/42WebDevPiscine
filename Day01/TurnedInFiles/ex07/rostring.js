873c75cca934c2a62f9577b19879607037b854aa
jcope <thejemmeh@gmail.com>
2019-03-27 23:52:38 -0700

    Explains

--- Day01/TurnedInFiles/ex07/rostring.js
+++ Day01/TurnedInFiles/ex07/rostring.js
@@ -1,5 +1,8 @@
+//this works but I was a few minutes late on submission.
+
 arr = [];
 if (process.argv[2])
 	arr = process.argv[2].trim().replace(/\s+/g,' ').split(/\s+/);
 
-console.log(arr.join(' '))
+arr.push(arr.shift());
+console.log(arr.join(' '));
