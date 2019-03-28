cd777bd1d7f540d8945ad404417fa947609c6cd6
jcope <thejemmeh@gmail.com>
2019-03-27 23:13:45 -0700

    ex06 complete

---
+++ Day01/TurnedInFiles/ex06/ssap.js
@@ -0,0 +1,10 @@
+arr = [];
+process.argv.forEach(function (param, index) {
+	if (index > 1)
+		arr = arr.concat(process.argv[index].trim().replace(/\s+/g,' ').split(/\s+/));
+});
+
+arr = arr.sort();
+arr.forEach(ele => console.log(ele));
+
+
