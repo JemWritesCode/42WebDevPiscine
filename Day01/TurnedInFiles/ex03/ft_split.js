27eba0df73204d44ebf575625a46f785cc874d37
jcope <thejemmeh@gmail.com>
2019-03-27 22:35:35 -0700

    Day01Ex03 complete

---
+++ Day01/TurnedInFiles/ex03/ft_split.js
@@ -0,0 +1,11 @@
+// type node ft_split.js in terminal to execute
+
+function ft_split(stringToSplit){
+	return(stringToSplit.split(/\s+/));
+}
+
+/*Main for Testing Below.
+
+console.log(ft_split(("Hello     World AAA")));
+
+*/
