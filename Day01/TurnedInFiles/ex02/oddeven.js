d59b6ece37daf1890e04cd5901717770278e9615
jcope <thejemmeh@gmail.com>
2019-03-27 19:03:03 -0700

    Day01 Ex02 resubmit, we aren't allowed to use readline

--- Day01/TurnedInFiles/ex02/oddeven.js
+++ Day01/TurnedInFiles/ex02/oddeven.js
@@ -1,24 +1,24 @@
 // Type node oddeven.js in the command line to execute
+// We aren't allowed to use readline for this exercise.
 
-const readline = require('readline').createInterface({
-	input: process.stdin,
-	output: process.stdout
-})
+var stdin = process.openStdin();
+var response = '';
 
-function AskQuestion(){
-	readline.question('Enter a number: ', (response) =>{
-		if(!response || isNaN(response)){
+process.stdout.write('Enter a number: ');
+stdin.addListener("data", function (d) {
+    // note:  d is an object, and when converted to a string it will
+    // end with a linefeed.  so we (rather crudely) account for that  
+    // with toString() and then trim() 
+    response = d.toString().trim();
+    if(!response || isNaN(response)){
 			console.log(`\'${response}\' is not a number`);
-		}else{
-			if(response % 2 == 0){
-			console.log(`The number ${response} is even`);
-			}
-			else{
-				console.log(`The number ${response} is odd`);
-			}
+	}else{
+		if(response % 2 == 0){
+		console.log(`The number ${response} is even`);
 		}
-		AskQuestion();
-	});
-}
-
-AskQuestion();
+		else{
+			console.log(`The number ${response} is odd`);
+		}
+	}
+	process.stdout.write('Enter a number: ');
+});
