709516387fdd1c703f0a530c6f0f4ddcadce6360
jcope <thejemmeh@gmail.com>
2019-03-28 20:13:22 -0700

    add explain

--- Day02/TurnInFiles/ex02/magnifying_glass.js
+++ Day02/TurnInFiles/ex02/magnifying_glass.js
@@ -1,1 +1,13 @@
 // Titles of Links and text between <a> tags should be changed to Uppercase.
+
+var fs = require('fs')
+fs.readFile(someFile, 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  var result = data.replace(/string to be replaced/g, 'replacement');
+
+  fs.writeFile(someFile, result, 'utf8', function (err) {
+     if (err) return console.log(err);
+  });
+});
