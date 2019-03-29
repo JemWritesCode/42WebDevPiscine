428b1bd62d1e9cf6a5d175565d0932196badc29c
jcope <thejemmeh@gmail.com>
2019-03-28 22:44:52 -0700

    Day02ex02 complete

--- Day02/TurnInFiles/ex02/magnifying_glass.js
+++ Day02/TurnInFiles/ex02/magnifying_glass.js
@@ -1,13 +1,23 @@
 // Titles of Links and text between <a> tags should be changed to Uppercase.
 
-var fs = require('fs')
-fs.readFile(someFile, 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-  var result = data.replace(/string to be replaced/g, 'replacement');
+if (process.argv[2]){
+	var fs = require('fs');
+	var text = fs.readFileSync(process.argv[2],'utf8')
 
-  fs.writeFile(someFile, result, 'utf8', function (err) {
-     if (err) return console.log(err);
-  });
-});
+	//Replace title="*"
+	result = text.replace(/<a.*?title="(.*?)">/g, function getSubstring(match){
+		return(match.replace(/[^title="]*".*/, function getUpper(submatch){
+			return(submatch = submatch.toUpperCase());
+		}));
+	})
+
+	//Replace words between <a></a>
+	result = result.replace(/<a.*</g, function getSubstring(match){
+		return(match.replace(/(>)(.*?)(<)/, function getUpper(submatch){
+			return(submatch = submatch.toUpperCase());
+		}));
+	})
+
+console.log(result)
+}
+
