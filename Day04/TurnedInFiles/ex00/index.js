7e43f11ef77065af27d7635c52befea7000902a0
jcope <thejemmeh@gmail.com>
2019-03-30 23:44:14 -0700

    Day04 done

--- Day04/TurnedInFiles/ex00/index.js
+++ Day04/TurnedInFiles/ex00/index.js
@@ -1,3 +1,5 @@
+// This doesn't work.
+
 //npm install express
 //npm install ejs
 //npm install cookie-parser
@@ -7,6 +9,7 @@ const express = require('express'); //Express is used for the server framework
 const ejs = require('ejs'); // EJS is used for the Template Engine. It allows the use of variables in the Markup(layout.ejs).
 const cookieParser = require('cookie-parser'); //Used for cookie to store the Login/Password
 const session = require('express-session');
+const bodyParser = require('body-parser');
 
 const app = express()
 const port = 3000
@@ -14,6 +17,7 @@ const port = 3000
 // APP USE/SET
 app.set('view engine', 'ejs');
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(session({
   secret: 'keyboard cat',
@@ -24,28 +28,36 @@ app.use(session({
 let sessions = {};
 
 
-// APP
+// APP GET /
 app.get('/',(req, res) => {
 	CookieLogin = '';
 	CookiePass = '';
 
-	if (req.cookies && req.cookies.NODESESSID && sessions[req.cookies.NODESESSID]) {
-		CookieLogin = sessions[req.cookies.NODESESSID].login;
-		CookiePass = sessions[req.cookies.NODESESSID].passwd;
-	} else if (req.query.submit == "OK" && req.query.login && req.query.passwd){
-		sessions[req.sessionID] = {};
-		sessions[req.sessionID].login = req.query.login;
-		sessions[req.sessionID].passwd = req.query.passwd; 
-		CookieLogin =  req.query.login;
-		CookiePass =  req.query.passwd; 
+	//if (req.cookies && req.cookies.NODESESSID && sessions[req.cookies.NODESESSID]) {
+	//	CookieLogin = sessions[req.cookies.NODESESSID].login;
+	//	CookiePass = sessions[req.cookies.NODESESSID].passwd;
+	//} else if (req.query.submit == "OK" && req.query.login && req.query.passwd){
+	//	sessions[req.sessionID] = {};
+	//	sessions[req.sessionID].login = req.query.login;
+	//	sessions[req.sessionID].passwd = req.query.passwd; 
+	//	CookieLogin =  req.query.login;
+	//	CookiePass =  req.query.passwd; 
+	//	res.cookie('NODESESSID', req.sessionID);
+	//} else 
+	//	res.cookie('NODESESSID', req.sessionID);
+
+	if (req.query.submit == "OK" && req.query.login && req.query.passwd){
+		CookieLogin= req.query.login;
+		CookiePass = req.query.passwd;
 		res.cookie('NODESESSID', req.sessionID);
-	} else 
-		res.cookie('NODESESSID', req.sessionID);
+		res.cookie('UserName', CookieLogin);
+		res.cookie('Password', CookiePass);
+	}
+
 
 	// Set the Cookies.
-	res.cookie('NODESESSID', req.sessionID);
-	res.cookie('UserName', CookieLogin);
-	res.cookie('Password', CookiePass);
+
+
 
 	//Show Me The Cookies
 	console.log(req.cookies)
@@ -54,5 +66,14 @@ app.get('/',(req, res) => {
 
 	res.render('../layout.ejs', {login: CookieLogin, passwd: CookiePass})
 });
+
+//app.get('/clearcookie', function(req,res){
+//	res.clearCookie('NODESESSID', req.sessionID);
+//	res.clearCookie('UserName');
+//	res.clearCookie('Password');
+//     res.send('Cookie deleted');
+//});
+
+
 
 app.listen(port, () => console.log(`The app is listening on port ${port}!`))
