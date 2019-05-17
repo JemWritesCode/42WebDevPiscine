950ca4ddcecbf665beef566573d577843a94ffc2
jcope <thejemmeh@gmail.com>
2019-05-17 16:09:12 -0700

    DB Seed now deletes. Category Tags Work.

--- Rush00/shopping-cart/seed/ProductSeeder.js
+++ Rush00/shopping-cart/seed/ProductSeeder.js
@@ -3,14 +3,13 @@ var Product = require('../models/product');
 var mongoose = require('mongoose');
 const connectionString = 'mongodb+srv://jem:aBRY1wuriaitLrKf@cluster0-bu7rx.mongodb.net/Shop?retryWrites=true';
 
-mongoose.connect(connectionString);
+mongoose.connect(connectionString, { useNewUrlParser: false });
 
-//Delete things currently in this mongoDB cluster -- I want my seeder to be able to always start the db over.
-//var db = mongoose.connection;
-//db.on('error', console.error.bind(console, 'connection error:'));
-//db.once('open', function() {
-//  db.collection("test").remove()
-//});
+//Delete things currently in Shop.products. This seeder always clears the DB to start fresh.
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  db.collection("products").deleteMany({}, function(){
 
 var products = [
 	new Product({
@@ -18,70 +17,70 @@ var products = [
 		title: 'Dog Food',
 		description: 'Keeps Doggos Fed and Happy',
 		price: 10,
-		category: ['pets']
+		categoryTags: ['pets']
 	}),
 	new Product({
 		imagepath: 'https://github.com/Jemmeh/42WebDevPiscine/blob/master/Rush00/Images/animal-3174290_1920.jpg?raw=true',
 		title: 'Bird Food',
 		description: 'This is delicious, quoth the raven evermore',
 		price: 5,
-		category: ['pets']
+		categoryTags: ['pets']
 	}), 
 	new Product({
 		imagepath: 'https://github.com/Jemmeh/42WebDevPiscine/blob/master/Rush00/Images/cat-1136365_1920.jpg?raw=true',
 		title: 'Cat Food',
 		description: 'Felines feel fine feeding on this grub.',
 		price: 13,
-		category: ['pets']
+		categoryTags: ['pets']
 	}), 
 	new Product({
 		imagepath: 'https://raw.githubusercontent.com/Jemmeh/42WebDevPiscine/master/Rush00/Images/fish.jpg',
 		title: 'Fish Food',
 		description: 'We promise your fish will say yum!',
 		price: 4,
-		category: ['pets']
+		categoryTags: ['pets']
 	}), 
 	new Product({
 		imagepath: 'https://raw.githubusercontent.com/Jemmeh/42WebDevPiscine/master/Rush00/Images/business-suit-690048_1920.jpg',
 		title: 'Fancy Suit',
 		description: 'You will look dashing in this.',
 		price: 50,
-		category: ['clothing']
+		categoryTags: ['clothing']
 	}), 
 	new Product({
 		imagepath: 'https://github.com/Jemmeh/42WebDevPiscine/blob/master/Rush00/Images/girl-1333640_1920.jpg?raw=true',
 		title: 'Fancy Dress',
 		description: 'Ready for a night out with your pinky out.',
 		price: 75,
-		category: ['clothing']
+		categoryTags: ['clothing']
 	}), 
 	new Product({
 		imagepath: 'https://github.com/Jemmeh/42WebDevPiscine/blob/master/Rush00/Images/jeans-4050815_1920.jpg?raw=true',
 		title: 'Jeans',
 		description: 'Comfortable and durable.',
 		price: 30,
-		category: ['clothing']
+		categoryTags: ['clothing']
 	}), 
 	new Product({
 		imagepath: 'https://github.com/Jemmeh/42WebDevPiscine/blob/master/Rush00/Images/couch-2590993_1920.jpg?raw=true',
 		title: 'Moar Jeans',
 		description: 'Also comfortable and durable, but now with 100% more dog.',
 		price: 40,
-		category: ['clothing']
+		categoryTags: ['clothing']
 	}), 
 	new Product({
-		imagepath: 'https://github.com/Jemmeh/42WebDevPiscine/blob/master/Rush00/Images/jeans-4050815_1920.jpg?raw=true',
-		title: 'Jeans',
-		description: 'Comfortable and durable.',
+		imagepath: 'https://github.com/Jemmeh/42WebDevPiscine/blob/master/Rush00/Images/dog_supreme_480x.jpg?raw=true',
+		title: 'Dog Hoodie',
+		description: 'Your dog will be styling in this ultra soft hoodie.',
 		price: 30,
-		category: ['clothing']
+		categoryTags: ['clothing', 'pets']
 	}), 
 	new Product({
-		imagepath: 'https://github.com/Jemmeh/42WebDevPiscine/blob/master/Rush00/Images/couch-2590993_1920.jpg?raw=true',
-		title: 'Moar Jeans',
-		description: 'Also comfortable and durable, but now with 100% more dog.',
+		imagepath: 'https://github.com/Jemmeh/42WebDevPiscine/blob/master/Rush00/Images/catsuit.jpg?raw=true',
+		title: 'Cat Suit',
+		description: 'For the discerning cat.',
 		price: 40,
-		category: ['clothing']
+		categoryTags: ['clothing', 'pets']
 	})
 ];
 var done = 0;
@@ -97,3 +96,5 @@ for (var i=0; i < products.length; i++){
 function exit(){
 	mongoose.disconnect();
 }
+  })
+});
