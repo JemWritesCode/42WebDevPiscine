cc008e5b86aabef8353d27ab227f8c57c9ccaf8b
jcope <thejemmeh@gmail.com>
2019-05-28 00:07:11 -0700

    Video 14 complete

--- Rush00/shopping-cart/routes/index.js
+++ Rush00/shopping-cart/routes/index.js
@@ -40,4 +40,12 @@ router.get('/shopping-cart', function(req, res, next){
 	res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
 });
 
+router.get('/checkout', function(req, res, next){
+	if(!req.session.cart){
+		return res.redirect('/shopping-cart');
+	}
+	var cart = new Cart(req.session.cart);
+	res.render('shop/checkout', {total: cart.totalPrice});
+})
+
 module.exports = router;
