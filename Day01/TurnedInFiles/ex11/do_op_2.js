c9c8b10504dedec26cbdf45b2c777b47ed494703
jcope <thejemmeh@gmail.com>
2019-06-08 15:30:33 -0700

    Day01Ex11

--- Day01/TurnedInFiles/ex11/do_op_2.js
+++ Day01/TurnedInFiles/ex11/do_op_2.js
@@ -1,28 +1,14 @@
-function do_op_two (arg) {
-    
-    switch (op) {
-        case '+':
-            console.log(x + y);
-            break;
-        case '-':
-            console.log(x - y);
-            break;
-        case '/':
-            console.log(x / y);
-            break;
-        case '%':
-            console.log(x % y);
-            break;
-        case '*':
-            console.log(x * y);
-            break;
+function do_op_two () {
+    if (process.argv.length == 3) { //0 is node, 1 is do_op.js. So the param is 2. But length starts at 1, so needs to be 3 total (0, 1, 2).
+        try {
+            console.log(eval(process.argv[2].trim())); 
+        } catch (e) {
+                console.log("Syntax Error");
+        }
+    }
+    else{
+        console.log("Incorrect Parameters");
     }
 }
 
-if (process.argv.length == 3) { //0 is node, 1 is do_op.js. So the param is 2. But length starts at 1, so needs to be 3 total (0, 1, 2). 
-    console.log(process.argv[2].trim()); //testing only
-    do_op_two(process.argv[2].trim());
-}
-else{
-    console.log("Incorrect Parameters");
-}
+do_op_two();
