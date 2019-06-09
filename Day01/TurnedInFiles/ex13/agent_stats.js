082b903f71529c3452e2b16b134b9768f954364a
jcope <thejemmeh@gmail.com>
2019-06-09 16:43:00 -0700

    Day01EX13 Average Option Working

--- Day01/TurnedInFiles/ex13/agent_stats.js
+++ Day01/TurnedInFiles/ex13/agent_stats.js
@@ -1,14 +1,4 @@
-var evaluations = [];
-
-function readAndCreateObjectArray(){
-    function scoreObject(user, score, grader, feedback) {
-        this.user = user;
-        this.score = score;
-        this.grader = grader;
-        this.feedback = feedback;
-      }
-      
-    var temp;
+if (process.argv.length > 2){ // 1 is node, 2 is agent_stats.js. (argv.length is 1-indexed)
     var readline = require('readline');
 
     var rl = readline.createInterface({
@@ -18,40 +8,29 @@ function readAndCreateObjectArray(){
     });
     
     var linecounter = 0;
+    var numberOfGrades = 0;
+    var averageTotal = 0;
     rl.on('line', function (line) {
-        linecounter++;
-        if (linecounter > 1){
-          temp = line.split(";");
-          var currentScore = new scoreObject(temp[0], temp[1], temp[2], temp[3]);
-          evaluations.push(currentScore);
+        linecounter++; 
+        if (linecounter > 1){ // skip the column headers.
+          if (process.argv[2] == "average" || process.argv[2] == "moyenne"){
+            var temp;
+            temp = line.split(";"); // temp[0] = user, temp[1] = note/grade, temp[2] = Grader, temp[3] = Feedback
+            if (temp[2] != "moulinette" && temp[1] !== ''){
+              averageTotal += parseInt(temp[1]);
+              numberOfGrades++;
+            }
+          } else if(process.argv[2] == "average_user" || process.argv[2] == "moyenne_user"){
+        
+          } else if(process.argv[2] == "moulinette_variance" || process.argv[2] == "ecart_moulinette"){
+        
+          }
         }
     });
-    
-    /*Print the evaluations array of ScoreObjects.*/
-    rl.on('close', function () {
-        console.dir(evaluations, {'maxArrayLength': null} );
-    });
-}
 
-function averageGrades(){
-  var usersScores = [];
-  for(var i=0; i<evaluations.length; i++) {
-    for(user in evaluations[i]) {
-      if(evaluations[i][key].indexOf(toSearch)!=-1) {
-        usersScores.push(evaluations[i]);
+    rl.on('close', function(){
+      if (process.argv[2] == "average" || process.argv[2] == "moyenne"){
+        console.log((averageTotal / numberOfGrades).toFixed(13));
       }
-    }
-  }
-  console.log("here we go");
-}
-
-if (process.argv.length > 2){ // 1 is node, 2 is agent_stats.js. (argv.length is 1-indexed)
-  readAndCreateObjectArray();
-  if (process.argv[2] == "average" || process.argv[2] == "moyenne"){
-    averageGrades();
-  } else if(process.argv[2] == "average_user" || process.argv[2] == "moyenne_user"){
-
-  } else if(process.argv[2] == "moulinette_variance" || process.argv[2] == "ecart_moulinette"){
-
-  }
+    })
 }
